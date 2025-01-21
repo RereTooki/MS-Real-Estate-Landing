ae59c6381b1a2c70db4aa1058688c72bf8e0fb44
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-01-21 07:35:11 +0100

    added animations to discover section

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -9,29 +9,33 @@ import call from "../assets/icons/call.svg";
 import width from "../assets/icons/width.svg";
 import bathtub from "../assets/icons/bathtub.svg";
 import bedroom from "../assets/icons/bedroom.svg";
-import ellipses from "../assets/icons/eliipses.svg";
+import ellipses from "../assets/icons/ellipses.svg";
 import location from "../assets/icons/location.svg";
 import clock from "../assets/icons/clock.svg";
 
 const Discover = () => {
   return (
     <>
-      <div className="h-[100vh] w-full border-4 border-black border-double px-4 py-14 flex flex-row justify-between select-none">
-        <div className="wborder-4 w-[38vw] rounded-[24px] ibmFont">
-          <div className=" hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
+      <div className="h-[100vh] w-full sborder-4 border-black border-double px-4 py-14 flex flex-row justify-between select-none">
+        <div
+          className="wborder-4 w-[38vw] rounded-[24px] ibmFont relative overflow-hidden"
+          data-aos="fade-right"
+          data-aos-duration="1700"
+        >
+          <div className="hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
             <img
               className="w-full h-full rounded-[24px]"
               src={discover1}
               alt="background-img"
             />
           </div>
-          <div className=" text-black z-40 border-4 border-black w-[100%]">
+          <div className="border-4 border-black w-[100%] text-white absolute bottom-4 left-0 px-4 hover:scale-[1.02] transition ease-in-out duration-500 delay-10 cursor-pointer">
             <div className="flex flex-col gap-2">
               <div className="flex flex-col">
                 <h5 className="font-medium text-2xl">Modern Family Home</h5>
                 <h6 className="italic font-medium text-lg">$750,000</h6>
               </div>
-              <div className="flex items-center gap-4 font-normal text-xs text-[#fff]s">
+              <div className="flex items-center gap-4 font-normal text-xs">
                 <div className="borsder-2 h-[15px] w-[12px] object-cover">
                   <img
                     className="w-full h-full object-cover"
@@ -39,11 +43,15 @@ const Discover = () => {
                     alt="Location icon"
                   />
                 </div>
-                <p>Jalan Diponegoro No. 15, Semarang, Central Java 50241</p>
+                <p className="text-white/80">
+                  Jalan Diponegoro No. 15, Semarang, Central Java 50241
+                </p>
               </div>
               <div className="flex flex-row sborder-4 items-center justify-between mt-2">
-                <div className="sborder-2 flex justify-between w-1/2 text-[#fff]s">
-                  <p className="font-medium text-[8px]">@budi.santoso</p>
+                <div className="sborder-2 flex justify-between w-1/2">
+                  <p className="font-medium text-[8px] text-white/70">
+                    @budi.santoso
+                  </p>
                   <div className="flex gap-2">
                     {" "}
                     <div className="borsder-2 h-[10px] w-[10px] object-cover">
@@ -58,7 +66,7 @@ const Discover = () => {
                     </p>
                   </div>
                 </div>
-                <div className="borsder-2 h-[15px] w-[15px] object-cover mr-4">
+                <div className="borsder-2 h-[5px] w-[25px] object-cover mr-4">
                   <img
                     className="w-full h-full object-cover"
                     src={ellipses}
@@ -69,7 +77,11 @@ const Discover = () => {
             </div>
           </div>
         </div>
-        <div className="sborder-4 border-black flex flex-col justify-between ibmFont">
+        <div
+          className="sborder-4 border-black flex flex-col justify-between ibmFont"
+          data-aos="zoom-in"
+          data-aos-duration="1700"
+        >
           {" "}
           <div className="flex flex-col items-center gap-2 sborder-4 border-dashed">
             <div className="sborder-2 h-[60px] w-[60px] rounded-full hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
@@ -110,7 +122,7 @@ const Discover = () => {
             </div>
           </div>
           <div className="flex flex-col items-center gap-2 sborder-4 border-dashed h-full justify-around">
-            <div className="flex sborder-4 items-center gap-4">
+            <div className="flex sborder-4 items-center gap-4 hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
               <div className="h-[13px] w-[30px]">
                 <img
                   className="w-full h-full"
@@ -124,7 +136,7 @@ const Discover = () => {
               </div>
             </div>
             <div className="flex border-black sborder-4 w-full justify-around">
-              <div className="flex flex-col sborder-4 items-center gap-4">
+              <div className="flex flex-col sborder-4 items-center gap-4 hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
                 <div className="h-[24px] w-[24px]">
                   <img className="w-full h-full" src={bathtub} alt="bathtub" />
                 </div>
@@ -135,7 +147,7 @@ const Discover = () => {
                   <p className="italic text-xs font-medium">2 Rooms</p>
                 </div>
               </div>
-              <div className="flex flex-col sborder-4 items-center gap-4">
+              <div className="flex flex-col sborder-4 items-center gap-4 hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
                 <div className="h-[24px] w-[24px]">
                   <img className="w-full h-full" src={bedroom} alt="bathtub" />
                 </div>
@@ -149,7 +161,11 @@ const Discover = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between s-[0.5px] border-[#010101] w-[38vw] rounded-[24px]">
+        <div
+          className="flex flex-col justify-between border-[#010101] w-[38vw] rounded-[24px] ibmFont"
+          data-aos="fade-left"
+          data-aos-duration="1700"
+        >
           <div className="border-[0.5px] border-[#010101] w-[38vw]  rounded-[24px] flex flex-col px-4 py-6 ">
             <h3 className="ibmFont text-4xl font-medium leading-[47px] pb-4">
               Discover Your Next Chapter in the Perfect Home
@@ -162,20 +178,109 @@ const Discover = () => {
               aspirations.
             </p>
           </div>
-          <div className="flex flex-row bsorder-4 justify-between">
-            <div className="w-[48%] hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
-              <img
-                className="w-full h-full rounded-[24px]"
-                src={discover2}
-                alt="background-img"
-              />
+          <div className="flex flex-row sborder-4 justify-between">
+            <div className="w-[48%] relative overflow-hidden rounded-[24px]">
+              <div className="hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
+                <img
+                  className="w-full h-full rounded-[24px]"
+                  src={discover2}
+                  alt="Luxury Villa With Pool"
+                />
+              </div>
+
+              <div className="sborder-4 border-black w-[100%] text-white absolute bottom-4 left-0 px-4 hover:scale-[1.02] transition ease-in-out duration-500 delay-10 cursor-pointer">
+                <div className="flex flex-col gap-2">
+                  <div className="flex flex-col">
+                    <h5 className="font-medium text-xs leading-loose">
+                      Luxury Villa with Pool
+                    </h5>
+                    <h6 className="font-medium text-[10px]">$220,000</h6>
+                  </div>
+                  <div className="flex items-center gap-4 font-normal text-[8px]">
+                    <div className="borsder-2 h-[10px] w-[8px] object-cover">
+                      <img
+                        className="w-full h-full object-cover"
+                        src={location}
+                        alt="Location icon"
+                      />
+                    </div>
+                    <p className="truncate ... text-white/80">
+                      Jalan Gajah Mada No. 22, Ungaran, Central Java 50512
+                    </p>
+                  </div>
+                  <div className="flex flex-row sborder-4 items-center justify-between">
+                    <p className="font-medium text-[8px] text-white/70">
+                      @anisa.pratiwi
+                    </p>
+
+                    <div className="flex gap-2">
+                      {" "}
+                      <div className="borsder-2 h-[10px] w-[10px] object-cover">
+                        <img
+                          className="w-full h-full object-cover"
+                          src={clock}
+                          alt="A clock"
+                        />
+                      </div>
+                      <p className="text-[#D3D3D3] italic font-medium text-[8px]">
+                        9:15 AM
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="w-[48%] hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer">
-              <img
-                className="w-full h-full rounded-[24px]"
-                src={discover3}
-                alt="background-img"
-              />
+            <div className="w-[48%] relative overflow-hidden rounded-[24px]">
+              <div className="hover:scale-[1.04] transition ease-in-out duration-500 delay-10 cursor-pointer ">
+                <img
+                  className="w-full h-full rounded-[24px] "
+                  src={discover3}
+                  alt="Spacious Land For Development"
+                />
+                {/* Gradient Overlay */}
+                <div className="absolute inset-0 bg-gradient-to-b from-[#01010100] to-[#090909] rounded-[24px]"></div>
+              </div>
+              <div className="sborder-4 border-black w-[100%] text-white absolute bottom-4 left-0 px-4 hover:scale-[1.02] transition ease-in-out duration-500 delay-10 cursor-pointer">
+                <div className="flex flex-col gap-2">
+                  <div className="flex flex-col">
+                    <h5 className="font-medium text-xs leading-loose">
+                      Spacious Land for Development
+                    </h5>
+                    <h6 className="font-medium text-[10px]">$350,000</h6>
+                  </div>
+                  <div className="flex items-center gap-4 font-normal text-[8px]">
+                    <div className="borsder-2 h-[10px] w-[8px] object-cover">
+                      <img
+                        className="w-full h-full object-cover"
+                        src={location}
+                        alt="Location icon"
+                      />
+                    </div>
+                    <p className="truncate ... text-white/80">
+                      Jalan Siliwangi No. 10, Tegal, Central Java 52115
+                    </p>
+                  </div>
+                  <div className="flex flex-row sborder-4 items-center justify-between">
+                    <p className="font-medium text-[8px] text-white/70">
+                      @dedi.kurniawan
+                    </p>
+
+                    <div className="flex gap-2">
+                      {" "}
+                      <div className="borsder-2 h-[10px] w-[10px] object-cover">
+                        <img
+                          className="w-full h-full object-cover"
+                          src={clock}
+                          alt="A clock"
+                        />
+                      </div>
+                      <p className="text-[#D3D3D3] italic font-medium text-[8px]">
+                        10:00 AM
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
